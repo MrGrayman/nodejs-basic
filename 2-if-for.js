cb52fb93a6f8a66493060058de528699f2bcb2da
MrGrayman <gunboy.2541@gmail.com>
2019-09-21 12:19:18 +0700

    commit 1-4 file

---
+++ 2-if-for.js
@@ -0,0 +1,38 @@
+// const a = "a";
+// if (a === "a") {
+//     console.log("It's a");
+// } else {
+//     console.log("It's not a");
+// }
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+// console.log(i);
+
+// let i = 1;
+// let n = 0;
+// while (i <= 1000) {
+//     n = n + i;
+//     i++;
+// }
+// console.log(n);
+
+let a = 1;
+let b = 1.1;
+let c = "hello";
+let d = true;
+let e = false;
+let f = ["a", "b", "c"];
+f[0] = "f";
+f.push("h");
+console.log(f);
+let h = [];
+let student = {
+    id: "60146313",
+    name: "Chatchawal",
+    faculty: "Science"
+};
+console.log(student.name);
+student.name = "Gun";
+console.log(student["name"]);
---
+++ 4-class.js
@@ -0,0 +1,18 @@
+class Calculate {
+    static sum(x, y) {
+        return x + y;
+    }
+
+    sum2(x, y) {
+        return x + y;
+    }
+
+    static multiply(x, y) {
+        return x * y;
+    }
+}
+// const calculatorObject = new Calculate();
+// console.log(Calculate.sum(1, 2));
+// console.log(Calculate.sum2(2, 3));
+// console.log(Calculate.sum(5, 6));
+console.log(Calculate.multiply(5, 6));
